8f441071a172afbf4a634c0a66564084d3997f5a
Kumarmangalam Mishra <63307990+mangalam-123@users.noreply.github.com>
2022-05-20 13:35:04 +0530

    Update c10-route53-dns-registration.tf

--- Iac-devops/c10-route53-dns-registration.tf
+++ Iac-devops/c10-route53-dns-registration.tf
@@ -1,11 +1,11 @@
 # Register a DNS for route53 and create an alias for loadbalancer ad also an record set
 # dterraform validatefault Route
-variable "dns_name" {
+/*variable "dns_name" {
   description = "this is the dns name"
   type = string
   default = ""
 }
-
+*/
 resource "aws_route53_record" "myapp_dns" {
   
   zone_id = data.aws_route53_zone.mydomain.zone_id
